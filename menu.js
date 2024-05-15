195bb42ec757fa4eda31000b664b854c8489a7da
Riya Sharma <riyasharmasjcs@gmail.com>
2024-05-15 12:09:15 +0530

    Search bar added

--- menu.js
+++ menu.js
@@ -17,3 +17,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+const searchBox = document.querySelector('.search-box');
+const searchBtn = document.querySelector('.search-btn');
+const search = document.querySelector('.search');
+const closeBtn = document.querySelector('.close-btn');
+
+searchBtn.addEventListener('click', function(){
+  console.log(search.classList.contains('active'));
+  if(search.classList.contains('active')){
+    searchBox.value = ''
+  }
+  else {
+    search.classList.add('active');
+    searchBox.focus();
+  }
+})
+
+closeBtn.addEventListener('click', function(){
+  search.classList.remove('active');
+  searchBox.value = '';
+})
