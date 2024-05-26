34006f2fac0bb57763983bd8613fb4b788699361
Sakira Ali <sakiraali3430@gmail.com>
2024-05-26 15:17:16 +0530

    Upgraded Contact Us UI

--- menu.js
+++ menu.js
@@ -17,3 +17,25 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+// contact us page js
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
