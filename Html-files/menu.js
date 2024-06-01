eb304f6a544b9c0b81c3382aa136f968cfd490a7
Vansh Sardana <vanshsrdn25703@gmail.com>
2024-06-01 17:24:31 +0530

    Enhanced star rating functionality.

--- Html-files/menu.js
+++ Html-files/menu.js
@@ -1,7 +1,5 @@
-
 function rateItem(button, rating) {
     const starButtons = button.parentElement.querySelectorAll('.star-button');
-
     starButtons.forEach((starButton, index) => {
         if (index < rating) {
             starButton.classList.add('rated');
@@ -10,6 +8,45 @@ function rateItem(button, rating) {
         }
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const starButtons = document.querySelectorAll('.star-button');
+    starButtons.forEach((starButton, index) => {
+        //Adding event listener for all stars
+        starButton.addEventListener('mouseover', (e) => {
+            highlightStars(e.target, (index)%5);
+        });
+        starButton.addEventListener('mouseout', (e) => {
+            removeHoverStars(e.target);
+        });
+    });
+
+    function highlightStars(target, rating) {
+        //Finding the parent container of the current star
+        const starRating = target.closest('.star-rating');
+        const starChildren = Array.from(starRating.children);
+
+        starChildren.forEach((starButton, index) => {
+            if (index <= rating) {
+                //glowing up all stars before current index
+                starButton.classList.add('hover');
+            } else {
+                //(supressing the effect of rated class)
+                starButton.classList.add('greyed');
+            }
+        });
+    }
+    function removeHoverStars(target) {
+        const starRating = target.closest('.star-rating');
+        const starButtons = Array.from(starRating.children);
+        starButtons.forEach(starButton => {
+            //removing both hover and greyed classes in order to make 'rated' class stand out
+            starButton.classList.remove('hover');
+            starButton.classList.remove('greyed');
+        });
+    }
+});
+
 
 document.querySelectorAll('.add-to-cart-button').forEach(button => {
     button.addEventListener('click', function () {
