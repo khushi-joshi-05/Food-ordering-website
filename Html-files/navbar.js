ef0b86490d1965ac832bcb854dba9ce619721850
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-08-02 11:21:55 +0530

    resolved navbar THEME TOGGLING BUG from all pages

--- Html-files/navbar.js
+++ Html-files/navbar.js
@@ -6,10 +6,10 @@ themeToggleIcon.addEventListener("click", function () {
   document.body.classList.toggle(":root")
   if (document.body.classList.contains("dark-theme")) {
     themeToggleIcon.src = "../Images/navbar/sun.png";
-    themeToggleLogo.src = "Images/logo/Logo-Dark.png";
+    themeToggleLogo.src="../Images/logo/Logo-Light.png";
   } else {
     themeToggleIcon.src = "../Images/navbar/moon.png";
-    themeToggleLogo.src="Images/logo/Logo-Light.png";
+    themeToggleLogo.src = "../Images/logo/Logo-Dark.png";
   }
 });
 
